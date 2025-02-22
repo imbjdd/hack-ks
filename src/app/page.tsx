a6ecf5be963384f875ed7638673597dea95bd98e
imbjdd <b0bb1e@protonmail.com>
2025-02-23 00:59:43 +0100

    fix: build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import Image from "next/image";
 import { animate } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
 
-function Counter({ from, to }) {
-  const nodeRef = useRef();
+function Counter({ from, to }:{from:any, to:any}) {
+  const nodeRef = useRef<HTMLDivElement>(null);
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    const node = nodeRef.current;
+    const node:any = nodeRef.current;
 
     const observer = new IntersectionObserver(
       (entries) => {
@@ -44,7 +43,7 @@ function Counter({ from, to }) {
     const controls = animate(from, to, {
       duration: 1.5,
       onUpdate(value) {
-        node.textContent = value.toFixed(0);
+        if(node) node.textContent = value.toFixed(0);
       },
     });
 
@@ -90,7 +89,7 @@ export default function Home() {
           <span>Le premier Hackathon IA</span><br/>
           <span className="text-[#FF2727]">par KryptoSphère</span>
         </h1>
-        <p className="text-xl">Tentez votre chance en participant à notre hackathon de 48 heures.<br/>Obtenez de l’aide de la part de mentors.</p>
+        <p className="text-xl">Tentez votre chance en participant à notre hackathon de 48 heures.<br/>Obtenez de l&apos;aide de la part de mentors.</p>
         <button className="py-4 px-20 bg-[#612DFC] w-fit text-white font-bold">Participer</button>
       </div>
 
@@ -108,7 +107,7 @@ export default function Home() {
       <div className="bg-[#FF2727] text-white min-h-80 w-full flex flex-col items-center py-12 px-4 md:px-0">
       <div className="container flex flex-col gap-2 px-4 md:px-0">
         <p className="font-bold text-xl">Notre équipe</p>
-        <p className="text-white/80">Composé d'experts.</p>
+        <p className="text-white/80">Composé d&apos;experts.</p>
         <div className="flex flex-col items-center md:flex-row pt-2 gap-8 px-8 md:px-0">
           {team.map((personne, index:number) => (
             <div className="flex flex-col" key={index}>
@@ -125,7 +124,7 @@ export default function Home() {
 
       <div className="container py-4 md:py-20 px-4 md:px-0 flex flex-col gap-2">
         <p className="font-bold text-xl">Le lieu</p>
-        <p className="text-black/70">Le hackathon se déroulera dans l'enceinte de l'école ESSEC.</p>
+        <p className="text-black/70">Le hackathon se déroulera dans l&apos;enceinte de l&apos;école ESSEC.</p>
         <iframe className="w-full min-h-96" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5231.603174087223!2d2.075699076864314!3d49.03338058837544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6f53ae44bd367%3A0xa3f3ee2330ea2664!2sESSEC%20Business%20School%20-%20Campus%20de%20Cergy-Pontoise!5e0!3m2!1sfr!2sfr!4v1740253142193!5m2!1sfr!2sfr" loading="lazy"></iframe>
       </div>
     </div>
