9ef67ff2c3b751b0b3a95f236aca3a65509a5016
imbjdd <b0bb1e@protonmail.com>
2025-02-22 23:28:03 +0100

    feat: first init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,133 @@
+'use client'
+
 import Image from "next/image";
+import { animate } from "framer-motion";
+import React, { useEffect, useRef, useState } from "react";
+
+function Counter({ from, to }) {
+  const nodeRef = useRef();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const node = nodeRef.current;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            observer.unobserve(node); // Arrête d'observer une fois que l'élément est visible
+          }
+        });
+      },
+      {
+        threshold: 0.1, // Déclenche l'observation lorsque 10% de l'élément est visible
+      }
+    );
+
+    if (node) {
+      observer.observe(node);
+    }
+
+    return () => {
+      if (node) {
+        observer.unobserve(node);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isVisible) return;
+
+    const node = nodeRef.current;
+
+    const controls = animate(from, to, {
+      duration: 1.5,
+      onUpdate(value) {
+        node.textContent = value.toFixed(0);
+      },
+    });
+
+    return () => controls.stop();
+  }, [from, to, isVisible]);
+
+  return <span ref={nodeRef} />;
+}
+
+function Star() {
+  return <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fillRule="evenodd" clipRule="evenodd" d="M4.72793 0.48528L0.485291 4.72792L8.97057 13.2132L0.485277 21.6985L4.72792 25.9411L13.2132 17.4558L21.6985 25.9411L25.9411 21.6985L17.4558 13.2132L25.9411 4.72792L21.6985 0.485284L13.2132 8.97056L4.72793 0.48528ZM13.2132 8.97056L8.97057 13.2132L13.2132 17.4558L17.4558 13.2132L13.2132 8.97056Z" fill="black"/>
+  </svg>  
+}
 
 export default function Home() {
+  const team = [
+    {name:'Salim Boujaddi', title: 'Head of Tech', image: 'salim.png'},
+    {name:'Salim Boujaddi', title: 'Head of Tech', image: 'salim.png'},
+    {name:'Salim Boujaddi', title: 'Head of Tech', image: 'salim.png'},
+    {name:'Salim Boujaddi', title: 'Head of Tech', image: 'salim.png'},
+    {name:'Salim Boujaddi', title: 'Head of Tech', image: 'salim.png'},
+    {name:'Salim Boujaddi', title: 'Head of Tech', image: 'salim.png'},
+  ]
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col items-center overflow-hidden">
+      <div className="flex justify-between container items-center px-4 md:px-0">
+          <div>
+            <img className="w-32" src="/ks.png" />
+          </div>
+          <div className="flex gap-4 md:gap-8">
+            <p>Hackathon</p>
+            <p>FAQ</p>
+            <p>About</p>
+          </div>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="container py-20 md:py-40 flex flex-col gap-8 px-4 md:px-0">
+        <h1 className="text-4xl md:text-8xl font-bold relative w-fit">
+          <div className="absolute top-0 right-0"><Star /></div>
+          <div className="absolute bottom-0 right-40 hidden md:block"><Star /></div>
+          <span className="text-[#612DFC]">From LLM to AI</span><br />
+          <span>Le premier Hackathon IA</span><br/>
+          <span className="text-[#FF2727]">par KryptoSphère</span>
+        </h1>
+        <p className="text-xl">Tentez votre chance en participant à notre hackathon de 48 heures.<br/>Obtenez de l’aide de la part de mentors.</p>
+        <button className="py-4 px-20 bg-[#612DFC] w-fit text-white font-bold">Participer</button>
+      </div>
+
+      <div className="bg-[#612DFC] min-h-80 w-full flex flex-col items-center py-12 px-4 md:px-0">
+        <div className="container">
+          <p className="text-white font-bold text-xl">Nos sponsors</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="container my-24 md:my-60 flex items-center justify-center">
+        <h2 className="text-5xl md:text-9xl font-bold w-fit relative">+<Counter from={0} to={30000} />€ de Prix
+        <div className="absolute top-0 -right-20"><Star /></div></h2>
+      </div>
+
+      <div className="bg-[#FF2727] text-white min-h-80 w-full flex flex-col items-center py-12 px-4 md:px-0">
+      <div className="container flex flex-col gap-2 px-4 md:px-0">
+        <p className="font-bold text-xl">Notre équipe</p>
+        <p className="text-white/80">Composé d'experts.</p>
+        <div className="flex flex-col items-center md:flex-row pt-2 gap-8 px-8 md:px-0">
+          {team.map((personne, index:number) => (
+            <div className="flex flex-col" key={index}>
+              <div className="bg-black p-2 mb-2">
+                <img className="md:w-40 aspect-square object-cover rounded-sm" src={'/'+personne.image}/>
+              </div>
+              <p className="font-bold">{personne.name}</p>
+              <p className="text-white/80">{personne.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      </div>
+
+      <div className="container py-4 md:py-20 px-4 md:px-0 flex flex-col gap-2">
+        <p className="font-bold text-xl">Le lieu</p>
+        <p className="text-black/70">Le hackathon se déroulera dans l'enceinte de l'école ESSEC.</p>
+        <iframe className="w-full min-h-96" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5231.603174087223!2d2.075699076864314!3d49.03338058837544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6f53ae44bd367%3A0xa3f3ee2330ea2664!2sESSEC%20Business%20School%20-%20Campus%20de%20Cergy-Pontoise!5e0!3m2!1sfr!2sfr!4v1740253142193!5m2!1sfr!2sfr" loading="lazy"></iframe>
+      </div>
     </div>
   );
 }
